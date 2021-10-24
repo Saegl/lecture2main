9947c352f26049860faa7656ac3768798ce48b72
Saegl <saegl@protonmail.com>
2021-10-24 21:52:32 +0600

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Saegl/lecture2main
+
+go 1.17
+
+require github.com/Saegl/lecture2lib v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Saegl/lecture2lib"
+)
+
+func main() {
+	fmt.Printf("Swap case of 'A' %c\n", lecture2lib.ChangeCharCase('A'))
+	fmt.Printf("Swap case of 'z' %c\n", lecture2lib.ChangeCharCase('z'))
+}
